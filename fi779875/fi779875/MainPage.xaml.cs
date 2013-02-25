cbe0f972a50da05db085fbf81cc2d8ea5c69881c
mrange <marten_range@hotmail.com>
2013-02-25 22:04:32 +0100

    1. Initial commit of Feedback Item 779875

---
+++ fi779875/fi779875/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿// ----------------------------------------------------------------------------------------------
+// Copyright (c) Mårten Rånge.
+// ----------------------------------------------------------------------------------------------
+// This source code is subject to terms and conditions of the Microsoft Public License. A 
+// copy of the license can be found in the License.html file at the root of this distribution. 
+// If you cannot locate the  Microsoft Public License, please send an email to 
+// dlr@microsoft.com. By using this source code in any fashion, you are agreeing to be bound 
+//  by the terms of the Microsoft Public License.
+// ----------------------------------------------------------------------------------------------
+// You must not remove this notice, or any other, from this software.
+// ----------------------------------------------------------------------------------------------
+
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace fi779875
+{
+    public sealed partial class MainPage
+    {
+        public string Testing {get;set;}
+
+        public MainPage()
+        {
+            InitializeComponent();
+            Testing = "Testing";
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+    }
+}
