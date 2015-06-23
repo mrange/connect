ecfaa1c9201ec06a9fe19e9b35452a6954c1c4e9
mrange <marten_range@hotmail.com>
2015-06-23 20:35:48 +0200

    ICE

---
+++ VS2015RC/ICEICE/ICEICE.cpp
@@ -0,0 +1,42 @@
+
+#include "stdafx.h"
+
+#include <type_traits>
+#include <vector>
+
+// Uncomment for a workaround in this specific case
+#define WORKAROUND
+
+namespace
+{
+  auto collect = [] (auto && collector)
+  {
+#ifndef WORKAROUND
+    using collector_type = decltype (collector);
+#endif
+
+    return
+      [collector] (auto && source)
+      {
+        using source_type       = std::remove_reference_t<decltype (source)>    ;
+        using value_type        = typename source_type::value_type              ;
+#ifdef WORKAROUND
+        using collector_type = decltype (collector);
+#endif
+        using inner_source_type = std::result_of_t<collector_type (value_type)> ;
+
+        return 1;
+      };
+
+  };
+}
+
+int main ()
+{
+  std::vector<int> ints;
+
+  auto c = collect ([] (auto && v) {return v;});
+  auto r = c (ints);
+
+  return 0;
+}
