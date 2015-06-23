22cd3eca510a353eb612dc5a3940cb23fd78b960
mrange <marten_range@hotmail.com>
2015-06-23 21:32:27 +0200

    New ICE

--- VS2015RC/ICEICE/ICEICE.cpp
+++ VS2015RC/ICEICE/ICEICE.cpp
@@ -1,44 +1,59 @@
 
 #include "stdafx.h"
 
+#include <iostream>
 #include <type_traits>
 #include <vector>
 
-// Uncomment for a workaround in this specific case
-// #define WORKAROUND
+// Set WORKAROUND to 1 or 2 for different workarounds
+#define WORKAROUND 0
 
 namespace
 {
-  auto collect = [] (auto && collector)
+  struct user
   {
-#ifndef WORKAROUND
-    using collector_type = decltype (collector);
-#endif
-
-    return
-      [collector] (auto && source)
-      {
-        using source_type       = std::remove_reference_t<decltype (source)>    ;
-        using value_type        = typename source_type::value_type              ;
-#ifdef WORKAROUND
-        using collector_type = decltype (collector);
-#endif
-        using inner_source_type = std::result_of_t<collector_type (value_type)> ;
-
-        return 1;
-      };
-
+    std::uint64_t id;
   };
+
+  template<typename TValueType>
+  struct strip_type
+  {
+    using type = std::remove_cv_t<std::remove_reference_t<TValueType>>  ;
+  };
+
+  template<class TValueType>
+  using strip_type_t = typename strip_type<TValueType>::type;
+
+  auto do_it =
+    [] (auto && source)
+    {
+      using source_type           = decltype (source)         ;
+      using stripped_source_type  = strip_type_t<source_type> ;
+
+#if WORKAROUND == 1
+      return [source = std::forward<source_type> (source)] (int sink)
+#else
+      return [source = std::forward<source_type> (source)] (auto && sink)
+#endif
+        {
+#if WORKAROUND == 2
+          using source_type           = decltype (source)         ;
+          using stripped_source_type  = strip_type_t<source_type> ;
+#endif
+          auto result = stripped_source_type ();
+  
+          return result;
+        };
+    };
 }
 
 int main ()
 {
-  std::vector<int> ints;
+  std::vector<user> users;
+  auto p = do_it (users);
+  std::vector<user> r = p (1);
 
-  auto c = collect ([] (auto && v) {return v;});
-  auto r = c (ints);
-
-  std::cout << r << std::endl;
+  std::cout << r.size () << std::endl;
 
   return 0;
 }
